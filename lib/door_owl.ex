9ed462025a43566e1ed49b8fe232a3f5c25edbcd
Odin <kammerloher.andreas@gmail.com>
2019-12-01 13:51:45 +0100

    Remove unused hello-world code

--- lib/door_owl.ex
+++ lib/door_owl.ex
@@ -1,18 +1,3 @@
 defmodule DoorOwl do
-  @moduledoc """
-  Documentation for DoorOwl.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DoorOwl.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
