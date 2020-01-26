29e423b9a07ba78cbe128d2e221ed9c2902a692e
Odin <kammerloher.andreas@gmail.com>
2020-01-26 22:06:26 +0100

    Add comment of data structure example for scan result, reduce time between scans

--- lib/door_owl/tag_detector.ex
+++ lib/door_owl/tag_detector.ex
@@ -22,9 +22,16 @@ defmodule DoorOwl.TagDetector do
     {:ok, state}
   end
 
+  # %Harald.HCI.Event.LEMeta.AdvertisingReport.Device{
+  # address: 254479696499413,
+  # address_type: 1,
+  # data: [{1, <<6>>}, {6, <<232, 191, 108, 207, 23, 139, 50, 184, 76, 72, 106, 229, 0, 255, 186, 171>>}, {22, <<10, 24, 35, 4, 133, 124, 3>>}],
+  # event_type: 0,
+  # rss: 180}
+
   def handle_info(:scan, state) do
     scan_result = Harald.LE.scan(:bt)
-    Logger.debug("Original: #{inspect scan_result}")
+    Logger.debug("Original: #{inspect(scan_result)}")
     addr_rss = scan_result |> device_maps_to_addr_and_rss()
     Logger.debug("Scan result: #{inspect(addr_rss)}")
     addr_rss_red = Enum.find(addr_rss, fn {addr, _rss} -> addr == @red_tag_addr end)
@@ -38,7 +45,7 @@ defmodule DoorOwl.TagDetector do
   # Helpers
 
   defp schedule_scan() do
-    Process.send_after(self(), :scan, 2000)
+    Process.send_after(self(), :scan, 200)
   end
 
   defp device_maps_to_addr_and_rss(device_maps),
