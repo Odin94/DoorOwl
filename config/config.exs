d5276734f74f1cad6fbfb2bf3f359e10c034b9f1
Odin <kammerloher.andreas@gmail.com>
2020-02-01 16:29:27 +0100

    Fix led order in configs

--- config/config.exs
+++ config/config.exs
@@ -74,11 +74,10 @@ config :nerves_network, :default,
   ]
 
 config :door_owl,
-  led_pin_red: 4,
-  led_pin_white: 5,
+  led_pin_white: 4,
+  led_pin_red: 5,
   led_pin_green: 6,
   target: Mix.target()
-
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
